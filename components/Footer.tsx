cb92f472e569fc427b0af1fc1fc0d61af16ecb2a
Stanley Lai <stanley.lai.104@gmail.com>
2022-07-22 18:03:00 +0800

    Init commit

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="container">
+      <footer className="d-flex flex-row justify-content-center">
+        <small>Credited by Stanley Lai</small>
+      </footer>
+    </div>
+  );
+}
